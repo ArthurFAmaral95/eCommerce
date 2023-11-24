f7d1143cb1bd9e76c97663d3289bc798317ec4a4
Arthur Amaral <arthuramaral6@gmail.com>
2023-11-23 21:07:44 -0300

    login pop up

--- src/components/Header/Login/index.tsx
+++ src/components/Header/Login/index.tsx
@@ -1,9 +1,11 @@
+import { LoginPopUp } from '../../LoginPopUp'
 import './styles.css'
 
 export function Login() {
   return (
     <div className="login">
       <img src="./login-icon.svg" alt="login icon" />
+      <LoginPopUp/>
     </div>
   )
 }
---
+++ src/components/LoginPopUp/index.tsx
@@ -0,0 +1,11 @@
+import './styles.css'
+
+export function LoginPopUp() {
+  return (
+    <div className="login-pop-up">
+      <button id="login">Login</button>
+      <button id="register">Register</button>
+
+    </div>
+  )
+}
