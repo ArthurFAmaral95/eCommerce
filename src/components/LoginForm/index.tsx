b06c7e211099f6980ffbc5c0ab2219e9f66721ce
Arthur Amaral <arthuramaral6@gmail.com>
2023-12-02 10:44:33 -0300

    set up of LoginForm

--- src/components/LoginForm/index.tsx
+++ src/components/LoginForm/index.tsx
@@ -1,26 +1,58 @@
 import { useState } from 'react'
+
+import { useForm } from 'react-hook-form'
+import { z } from 'zod'
+import { zodResolver } from '@hookform/resolvers/zod'
+
 import './styles.css'
+
+type UserFormProps = z.infer<typeof userLoginFormSchema>
+
+const userLoginFormSchema = z.object({
+  email: z
+    .string()
+    .min(1, 'Please enter your email.')
+    .email('This field should be an e-mail address.'),
+  password: z.string().min(8, 'Password must be at least 8 characters.')
+})
 
 export function LoginForm() {
   const [showPassword, setShowPassword] = useState(false)
 
+  const {
+    register,
+    handleSubmit,
+    formState: { errors }
+  } = useForm<UserFormProps>({
+    resolver: zodResolver(userLoginFormSchema)
+  })
+
   function changePasswordVisibility() {
     setShowPassword(!showPassword)
   }
+
+  function handleUserLogin(data: UserFormProps) {
+    console.log(data)
+  }
+
   return (
-    <form id="login-form">
-      <div>
-        <label htmlFor="e-mail">E-mail</label>
-        <input type="email" name="e-mail" id="e-mail" required />
+    <form id="login-form" onSubmit={handleSubmit(handleUserLogin)}>
+      <div className="email">
+        <div className="errors">
+          <label htmlFor="e-mail">E-mail</label>
+          {errors.email && <span>{errors.email.message}</span>}
+        </div>
+        <input type="email" id="e-mail" {...register('email')} />
       </div>
       <div>
-        <label htmlFor="password">Password</label>
+        <div className="errors">
+          <label htmlFor="password">Password</label>
+          {errors.password && <span>{errors.password.message}</span>}
+        </div>
         <input
           type={showPassword ? 'text' : 'password'}
-          name="password"
           id="password"
-          minLength={8}
-          required
+          {...register('password')}
         />
         <img
           src="./open-eye.svg"
