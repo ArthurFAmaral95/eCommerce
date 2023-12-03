5e48ff8e29c14cbe2349a228ebf905a173031754
Arthur Amaral <arthuramaral6@gmail.com>
2023-12-03 12:35:33 -0300

    CheckoutPage purchase overview section

--- src/pages/CheckoutPage/index.tsx
+++ src/pages/CheckoutPage/index.tsx
@@ -5,21 +5,36 @@ import {
   Total,
   UserLoggedInProps
 } from '../../types/types'
+import { CheckoutOverviewListItem } from '../../components/CheckoutOverviewListItem'
 
 type CheckoutPageProps = CartProductArrayProps & Total & UserLoggedInProps
 
 export function CheckoutPage(props: CheckoutPageProps) {
+  const renderOverviewList: any = []
+
+  props.cartProducts.map(order => {
+    renderOverviewList.push(
+      <CheckoutOverviewListItem key={order.orderId} product={order} />
+    )
+  })
+
   return (
     <main id="checkout-page">
       <section id="purchase-overview">
         <h2>Purchase Overview</h2>
+        <ul className="overview-list">{renderOverviewList}</ul>
+
+        <p>Total: ${props.total}</p>
       </section>
+      <hr />
       <section id="delivery-address">
         <h2>Address</h2>
       </section>
+      <hr />
       <section id="payment-method">
         <h2>Payment</h2>
       </section>
+      <hr />
       <button>Finish Purchase</button>
     </main>
   )
