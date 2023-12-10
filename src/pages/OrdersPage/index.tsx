fb3c62035d8b3a355523d62907c04a8b98744c43
Arthur Amaral <arthuramaral6@gmail.com>
2023-12-10 16:35:33 -0300

    fetchOrders function

--- src/pages/OrdersPage/index.tsx
+++ src/pages/OrdersPage/index.tsx
@@ -1,11 +1,28 @@
-import { UserName } from '../../types/types'
+import { UserName, Order } from '../../types/types'
+import { useState, useEffect } from 'react'
+
+import axios from 'axios'
 
 type OrdersPageProps = UserName
 
 export function OrdersPage(props: OrdersPageProps) {
-  const userId = JSON.parse(localStorage.getItem('user')|| 'false').userId
+  const userId = JSON.parse(localStorage.getItem('user') || 'false').userId
+  const [userOrders, setUserOrders] = useState<Order[]>([])
 
-  console.log(userId)
+  useEffect(() => {
+    fetchOrders()
+  }, [])
+
+  console.log({ userId, userOrders })
+
+  function fetchOrders() {
+    axios
+      .get(`http://localhost:4001/orders/${userId}`)
+      .then(response => {
+        setUserOrders(response.data)
+      })
+      .catch(err => console.error(err))
+  }
 
   return (
     <main id="orders-page">
